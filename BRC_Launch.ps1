c691efa0aa2255872715b02eaa34bc1858bd067d
Pedro Oliva Rodrigues <pedroolivarodrigues@outlook.com>
2017-11-03 11:35:27 -0200

    New DEV scripts

---
+++ BRC_Launch.ps1
@@ -0,0 +1,73 @@
+﻿param(
+    [Parameter(Mandatory = $true)][string]$DeployPath,
+    [Parameter(Mandatory = $true)][string]$FilesPath,
+    [string]$Version = '0000',
+    [ValidateSet("lite", "full", "unix")][string]$Format = "lite"
+)
+
+
+if (!(Test-Path $DeployPath))
+{
+    Write-Host "Error, Deploy path not found" -b Red -f White
+    exit 1
+}
+elseif (!(Test-Path $FilesPath))
+{
+    Write-Host "Error, Files path not found" -b Red -f White
+    exit 2
+}
+
+''
+Write-Host "Version: $Version"
+Write-Host "Format: $Format"
+''
+ 
+$OLDPWD = $PWD.Path
+
+#clear deploy folder
+Set-Location "$DeployPath"
+#del .\*.*
+del .\tmp\* -Recurse 
+
+Write-Host "Coping files..." -NoNewline
+
+#xcopy "$FilesPath\*" "$DeployPath\tmp\*" /Y /S /F
+copy "$FilesPath\*" "$DeployPath\tmp\" -Force -Recurse
+
+Write-Host "Done"
+
+Write-Host "Creating base zip..." -NoNewline
+
+# base zip
+$baseZipName = "BRC_v$Version"
+Compress-Archive .\tmp\* .\"$baseZipName.zip" -Force
+
+Write-Host "Done"
+
+# Full
+if ($Format -eq 'full')
+{
+    Write-Host "Creating 'Full' zips..." -NoNewline
+
+    copy ./"$baseZipName.zip" ./"$baseZipName.Full-x86.zip"
+    copy .\ffmpegExes\ffmpeg-x86.exe .\ffmpeg.exe -Force
+    Compress-Archive .\ffmpeg.exe ./"$baseZipName.Full-x86.zip" -Update
+
+    copy "./$baseZipName.zip" ./"$baseZipName.Full-x64.zip"
+    copy .\ffmpegExes\ffmpeg-x64.exe .\ffmpeg.exe -Force
+    Compress-Archive .\ffmpeg.exe ./"$baseZipName.Full-x64.zip" -Update
+
+    Write-Host "Done"
+}
+elseif ($Format -eq 'unix')
+{
+    Write-host "Creating 'unix' zip..." -NoNewline
+
+    copy ./"$baseZipName.zip" ./"$baseZipName.Unix.zip"
+    Compress-Archive ..\extras\utilities\BRC_unix.sh ./"$baseZipName.Unix.zip" -Update
+    
+    Write-Host "Done"
+}
+
+
+echo "Launch script finished"
---
+++ BRC_PostBuild.ps1
@@ -0,0 +1,85 @@
+﻿param (
+    [string]$TargetName,
+    [string]$TargetDir,
+    [string]$ConfigName
+)
+
+$OLDPWD = $PWD.Path
+Set-Location "$TargetDir"
+
+$splitPath = $TargetDir.Split('\\')
+
+if ($ConfigName.Length -eq 0)
+{
+    $ConfigName = $splitPath[$splitPath.Length - 1].TrimEnd()
+}
+
+# Linux Vm Transfer folder
+$vmBuildsDir = "F:\PCsVirtuais\_transfer\VS\builds"
+
+#ILMerge path
+$ilMerge = "F:\DEV\_Tools\ILMerge\ILMerge.exe"
+
+# .NET reference paths
+$net_refPath_v452 = "${env:ProgramFiles(x86)}\Reference Assemblies\Microsoft\Framework\.NETFramework\v4.5.2"
+
+
+function Merge
+{
+    param (
+        [ValidateLength(1,99)]
+        [string[]] $Filter
+        
+    )
+
+    if (!(Test-Path $ilMerge))
+    {
+        Write-Host "Error, IlMerge exe not found" -b Red -f White
+        throw "ilMerge not found"
+    }
+    ''
+    echo 'Running IlMerge'
+    ''
+    # run IlMerge
+	Write-Output "$ilMerge /lib:$net_refPath_v452 /wildcards /targetplatform:v4 /target:winexe /out:$TargetName.all.exe $TargetName.exe $Filter"
+    & "$ilMerge" /lib:"$net_refPath_v452" /wildcards /targetplatform:v4 /target:winexe /out:$TargetName.all.exe "$TargetName.exe" $Filter
+
+    ''
+    echo 'Cleanining old files:'
+    ''
+
+    del $Filter
+
+    ren "$TargetName.all.pdb" "$TargetName.all.pdb.temp"
+    del *.pdb
+    ren "$TargetName.all.pdb.temp" "$TargetName.pdb"
+    
+    del "$TargetName.exe"
+    ren "$TargetName.all.exe" "$TargetName.exe"
+
+    del *.xml
+    
+    ''
+    echo 'MERGE DONE'
+}
+
+
+if ($ConfigName.Contains("Unix"))
+{
+    del "Microsoft.WindowsAPICodePack*.dll", *.xml
+    ''
+    Write-Host "ConfigName is $ConfigName"
+    $vmdSubDir = $ConfigName.Substring(0, $ConfigName.IndexOf(' '))
+    ''
+    xcopy .\* "$vmBuildsDir\$vmdSubDir\*" /Y /S /F
+}
+
+
+if ($ConfigName.Equals("Release"))
+{
+    Merge -Filter @('Microsoft.WindowsAPICodePack*.dll', 'System.ValueTuple.dll')
+} 
+
+Set-Location "$OLDPWD"
+
+echo "PostBuild Done"
