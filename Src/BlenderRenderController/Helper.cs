9596a17cb7e57abdc802d027aa04b9d803e5eaf4
Pedro Oliva Rodrigues <pedroolivarodrigues@outlook.com>
2018-01-08 10:29:56 -0200

    removed old 'ShowErrors'

--- Src/BlenderRenderController/Helper.cs
+++ Src/BlenderRenderController/Helper.cs
@@ -52,84 +52,6 @@ namespace BlenderRenderController
             }
         }
         
-        static public void ShowErrors(MessageBoxIcon icon, string fmt, params AppErrorCode[] errorCodes)
-        {
-            var errorText = "";
-
-            foreach (var errorCode in errorCodes)
-                errorText += GetErrorText(errorCode, fmt);
-
-            MessageBox.Show(
-                    errorText,
-                    "",
-                    MessageBoxButtons.OK,
-                    icon);
-
-            logger.Warn(errorText);
-        }
-        static public void ShowErrors(MessageBoxIcon icon, params AppErrorCode[] errorCodes)
-        {
-            ShowErrors(icon, string.Empty, errorCodes);
-        }
-        static public void ShowErrors(MessageBoxIcon icon, AppErrorCode errorCode)
-        {
-            ShowErrors(icon, string.Empty, errorCode);
-        }
-
-        static private string GetErrorText(AppErrorCode code, string arg1 = "")
-        {
-            var sb = new StringBuilder();
-
-            switch (code)
-            {
-                case AppErrorCode.BLENDER_PATH_NOT_SET:
-                    sb.AppendLine("Please set correct path to Blender.");
-                    break;
-                case AppErrorCode.FFMPEG_PATH_NOT_SET:
-                    sb.AppendLine("Please set correct path to FFmpeg.");
-                    break;
-                case AppErrorCode.BLEND_FILE_NOT_EXISTS:
-                    sb.AppendLine("File does not exists anymore.");
-                    sb.AppendLine("It was removed from the list of recent blends.");
-                    break;
-                case AppErrorCode.RENDER_FORMAT_IS_IMAGE:
-                    sb.AppendLine("The render format is " + arg1 + " image.");
-                    sb.AppendLine("You can render an image sequence with this tool but you will need to make a video with other SW.");
-                    break;
-                case AppErrorCode.BLEND_OUTPUT_INVALID:
-                    sb.AppendLine("Unable to read output path, using project location.");
-                    break;
-                case AppErrorCode.UNKNOWN_OS:
-                    sb.AppendLine("Could not identify operating system, BRC might not work properly.");
-                    break;
-            }
-
-            return sb.ToString();
-        }
-
-
-
-        //static public string SecondsToString(double seconds, bool digital = false)
-        //{
-        //    TimeSpan t = TimeSpan.FromSeconds(seconds);
-        //    string timeString;
-        //    if (!digital) {
-        //        timeString = string.Format("{0:D1}h {1:D1}m {2:D1}s {3:D1}ms",
-        //                        t.Hours,
-        //                        t.Minutes,
-        //                        t.Seconds,
-        //                        t.Milliseconds);
-        //    }
-        //    else
-        //    {
-        //        timeString = string.Format("{0:D2}:{1:D2}:{2:D2}",
-        //                        t.Hours,
-        //                        t.Minutes,
-        //                        t.Seconds);
-        //    }
-        //    return timeString;
-
-        //}
 
         static public IEnumerable<Control> FindControlsByTag(Control.ControlCollection controls, string key)
         {
@@ -157,22 +79,6 @@ namespace BlenderRenderController
             return controlsWithTags;
         }
 
-        //public static string GetChunksFolder(string blendOutput)
-        //{
-        //    return Path.Combine(blendOutput, "chunks");
-        //}
-        //public static string GetChunksFolder(BlendData blendData)
-        //{
-        //    return GetChunksFolder(blendData.OutputPath);
-        //}
-        //public static string GetChunkTxt(string blendOutput)
-        //{
-        //    return Path.Combine(GetChunksFolder(blendOutput), "chunklist.txt");
-        //}
-        //public static string GetChunkTxt(BlendData blendData)
-        //{
-        //    return GetChunkTxt(blendData.OutputPath);
-        //}
 
     }
 }
