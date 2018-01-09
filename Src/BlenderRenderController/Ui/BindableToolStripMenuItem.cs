2cc373083f4a01f52c37c14d800c857cdf0147e5
Pedro Oliva Rodrigues <pedroolivarodrigues@outlook.com>
2018-01-09 16:59:35 -0200

    added bindableTSMI

---
+++ Src/BlenderRenderController/Ui/BindableToolStripMenuItem.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.ComponentModel;
+using System.Windows.Forms;
+using System.Drawing;
+
+namespace BlenderRenderController.Ui
+{
+    public class BindableToolStripMenuItem : ToolStripMenuItem, IBindableComponent
+    {
+        ControlBindingsCollection _bindCollection;
+        BindingContext _bindContext;
+
+
+        public BindableToolStripMenuItem()
+            : base() { }
+
+        public BindableToolStripMenuItem(string text) 
+            : base(text) { }
+
+        public BindableToolStripMenuItem(Image image) 
+            : base(image) { }
+
+        public BindableToolStripMenuItem(string text, Image image) 
+            : base(text, image) { }
+
+        public BindableToolStripMenuItem(string text, Image image, params ToolStripItem[] dropDownItems) 
+            : base(text, image, dropDownItems) { }
+
+        public BindableToolStripMenuItem(string text, Image image, EventHandler onClick, Keys shortcutKeys) 
+            : base(text, image, onClick, shortcutKeys) { }
+
+        public BindableToolStripMenuItem(string text, Image image, EventHandler onClick)
+            : base(text, image, onClick) { }
+
+        public BindableToolStripMenuItem(string text, Image image, EventHandler onClick, string name)
+            : base(text, image, onClick, name) { }
+
+
+        [DesignerSerializationVisibility(DesignerSerializationVisibility.Content)]
+        public ControlBindingsCollection DataBindings
+        {
+            get
+            {
+                if (_bindCollection == null)
+                {
+                    _bindCollection = new ControlBindingsCollection(this);
+                }
+
+                return _bindCollection;
+            }
+        }
+
+        [Browsable(false)]
+        public BindingContext BindingContext
+        {
+            get
+            {
+                if (_bindContext == null)
+                {
+                    _bindContext = new BindingContext();
+                }
+
+                return _bindContext;
+            }
+
+            set => _bindContext = value;
+        }
+    }
+}
